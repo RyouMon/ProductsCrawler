604626892e0afece610bb1d3acb94c6ec7264e2e
Wen <l1048445160@live.com>
2020-02-25 14:02:27 +0800

    fix(itemloaders): fix SupremeLoader. 输出时替换title字段不合法的字符。 输出时将images中所有链接的'thumb'替换为'sqr'。

--- GoodsCrawler/itemloaders.py
+++ GoodsCrawler/itemloaders.py
@@ -1,10 +1,15 @@
 from scrapy.loader import ItemLoader
-from scrapy.loader.processors import TakeFirst, Compose, Identity
+from scrapy.loader.processors import *
 
 
 class GoodsLoader(ItemLoader):
-    default_output_processor = TakeFirst()
+    default_output_processor = TakeFirst() # 默认使用SelectorList.get()方法
 
 
 class SupremeLoader(GoodsLoader):
-    pass
+    title_out = Compose(
+        TakeFirst(),
+        lambda x: x.replace("*", "-"),
+        lambda x: x.replace("/", "-"),
+    )
+    images_out = MapCompose(lambda x: x.replace("thumb", "sqr"))  # 把缩略图的链接替换为源图的链接
