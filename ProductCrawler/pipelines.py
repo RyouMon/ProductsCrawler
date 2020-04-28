925a55577831048372428c4a59c14dae120e31c9
Wen <l1048445160@live.com>
2020-04-28 13:31:55 +0800

    fix(Product/pipelines.py): moved two tool functions to utils.py.

--- ProductCrawler/pipelines.py
+++ ProductCrawler/pipelines.py
@@ -9,8 +9,7 @@ from scrapy import Request
 from scrapy.pipelines.images import ImagesPipeline
 from scrapy.exceptions import DropItem
 from ProductCrawler.settings import IMAGES_STORE
-
-ILLEGAL_FILENAME_CHARS = {"?", "/", "\\", ":", "*", ">", "<", "\""}
+from ProductCrawler.utils import file_path, legal_name
 
 
 class ProductUrlPipeline(object):
@@ -46,7 +45,7 @@ class ProductImagesPipeline(ImagesPipeline):
         image_base_url = item.get('image_base_url', None)
         if image_base_url:
             images_url = [image_base_url + part_url for part_url in item['images']]
-        else: # image_base_url is None
+        else:  # image_base_url is None
             images_url = item['images']
         for image_url in images_url:
             yield Request(image_url,
@@ -57,50 +56,3 @@ class ProductImagesPipeline(ImagesPipeline):
                                 'week': item.get('week'),
                                 'category': item.get('category')}
                           )
-
-
-def file_path(info, filename=None):
-    """
-    传入一个包含Item信息的字典，为商品选择一个保存的位置
-    :param info: Request.meta or item object.
-    :param filename: file name.
-    :return: 相对于IMAGE_STORE的路径
-    """
-    brand = info['brand']
-    title = info.get('title')
-    art_no = info.get('art_no')
-    season = info.get('season')
-    week = info.get('week')
-    category = info.get('category')
-    filepath = ''
-    if brand:
-        filepath += brand + '/'
-    if season:
-        filepath += season + '/'
-    if week:
-        filepath += week + '/'
-    elif category:
-        filepath += category + '/'
-    if art_no:
-        filepath += art_no + '/'
-    elif title:
-        filepath += title + '/'
-    else:
-        raise TypeError("至少要提供title与art_no的其中一个")
-    if filename:
-        filepath += filename
-    return filepath
-
-
-def legal_name(filename):
-    """
-    返回合法的文件名
-    :param filename: 可能包含非法字符的文件名字符串。
-    :return: 合法的文件名字符串。
-    """
-    filename_char_set = set(filename)
-    illegal_chars = filename_char_set & ILLEGAL_FILENAME_CHARS
-    if illegal_chars:
-        for char in illegal_chars:
-            filename = filename.replace(char, "_")
-    return filename
