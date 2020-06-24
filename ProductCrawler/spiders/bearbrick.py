cfc89f4dd412e617a60724f558eca9f69af7f0c3
Wen <l1048445160@live.com>
2020-06-24 11:02:49 +0800

    feat(spiders/): new spider bearbrick.

---
+++ ProductCrawler/spiders/bearbrick.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class BearbrickSpider(CrawlSpider):
+    name = 'bearbrick'
+    allowed_domains = ['bearbrick.com']
+    start_urls = ['http://bearbrick.com/']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        item = {}
+        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
+        #item['name'] = response.xpath('//div[@id="name"]').get()
+        #item['description'] = response.xpath('//div[@id="description"]').get()
+        return item
