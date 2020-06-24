0f553075df152ab047cb0815befe5bf742b5b0df
Wen <l1048445160@live.com>
2020-06-24 13:40:21 +0800

    fix(crawl.py): fixed BREAKING CHANGE; removed useless code.

--- crawl.py
+++ crawl.py
@@ -5,15 +5,13 @@ import sys
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from ProductCrawler.arg_parser import parse
-from ProductCrawler.utils import spider_settings
 
 
 def run():
     name_space = parse.parse_args(sys.argv[1:])
     brand = name_space.brand
-    project_settings = get_project_settings()
-    project_settings.update(spider_settings(brand))
-    process = CrawlerProcess(project_settings)
+    settings = get_project_settings()
+    process = CrawlerProcess(settings)
 
     process.crawl(brand, **{'start_urls': name_space.start_urls})
     process.start()
