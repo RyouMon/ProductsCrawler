239d46037a8a8f81e04a4e4182f496b3b6d66810
Wen <l1048445160@live.com>
2020-06-29 14:54:57 +0800

    fix(ProductCrawler/spiders/supreme.py): use universal method parse_item and outer config to parse response.

--- ProductCrawler/spiders/supreme.py
+++ ProductCrawler/spiders/supreme.py
@@ -5,6 +5,7 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from ProductCrawler.itemloaders import SupremeLoader
 from ProductCrawler.items import ProductItem
+from ProductCrawler.spiders.universal_methods.parse_item import parse_item
 
 
 class SupremeSpider(CrawlSpider):
@@ -18,6 +19,7 @@ class SupremeSpider(CrawlSpider):
         self.start_urls = start_urls
         self.season = re.findall(r'season/(.*?)/', start_urls[-1]).pop()
         self.rules = ()
+        self.parse_item = parse_item
 
     def parse_start_url(self, response):
         """
@@ -52,17 +54,18 @@ class SupremeSpider(CrawlSpider):
         for detail_no in details:
             yield Request(
                 url=self.details_base_url+detail_no.get(),
-                callback=self.parse_item
+                callback=self.parse_item,
+                cb_kwargs={"spider": self}
             )
 
-    def parse_item(self, response):
-        loader = SupremeLoader(item=ProductItem(), response=response)
-        loader.add_value('brand', 'supreme')
-        loader.add_xpath('title', '//h1[@class="detail-title"]/text()')
-        loader.add_value('item_url', response.url)
-        loader.add_xpath('price', '//span[@class="price-label"]/text()')
-        loader.add_value('image_base_url', self.image_base_url)
-        loader.add_xpath('images', '//div[@class="carousel-inner"]//img/@src')
-        loader.add_value('season', self.season)
-        loader.add_xpath('week', '//h2[@class="details-release-small"]/span/text()')
-        yield loader.load_item()
+    # def parse_item(self, response):
+    #     loader = SupremeLoader(item=ProductItem(), response=response)
+    #     loader.add_value('brand', 'supreme')
+    #     loader.add_xpath('title', '//h1[@class="detail-title"]/text()')
+    #     loader.add_value('item_url', response.url)
+    #     loader.add_xpath('price', '//span[@class="price-label"]/text()')
+    #     loader.add_value('image_base_url', self.image_base_url)
+    #     loader.add_xpath('images', '//div[@class="carousel-inner"]//img/@src')
+    #     loader.add_value('season', self.season)
+    #     loader.add_xpath('week', '//h2[@class="details-release-small"]/span/text()')
+    #     yield loader.load_item()
