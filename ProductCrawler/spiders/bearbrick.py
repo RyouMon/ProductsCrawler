2f05f4abe546669aa76b9289f2d9a51e86d03fb7
Wen <l1048445160@live.com>
2020-07-04 11:55:48 +0800

    fix(ProductCrawler/spiders/bearbrick.py): implemented spider bearbrick.

--- ProductCrawler/spiders/bearbrick.py
+++ ProductCrawler/spiders/bearbrick.py
@@ -1,21 +1,34 @@
 # -*- coding: utf-8 -*-
-import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from ProductCrawler.spiders.universal_methods.parse_item import parse_item
 
 
 class BearbrickSpider(CrawlSpider):
     name = 'bearbrick'
     allowed_domains = ['bearbrick.com']
-    start_urls = ['http://bearbrick.com/']
-
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        # matching product page, callback with parse_item
+        Rule(
+            LinkExtractor(
+                allow=r'.*html',
+                restrict_xpaths='//section[@id="itemList"]'
+            ), callback='call_parse_item', follow=False
+        ),
+        # matching other pages
+        Rule(
+            LinkExtractor(
+                restrict_xpaths='//div[@id="pager"]',
+            ),
+            follow=True
+        )
     )
 
-    def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+    def __init__(self, start_urls=None):
+        super(BearbrickSpider, self).__init__()
+        self.parse_item = parse_item
+        if start_urls:
+            self.start_urls = start_urls
+
+    def call_parse_item(self, response):
+        return parse_item(response, self)
