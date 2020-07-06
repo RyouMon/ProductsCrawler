c79d919745f7f957589a6c8cbd63c215769ea03b
Wen <l1048445160@live.com>
2020-07-06 09:15:33 +0800

    fix(ProductCrawler/spiders/uastore.py): implemented spider uastore.

--- ProductCrawler/spiders/uastore.py
+++ ProductCrawler/spiders/uastore.py
@@ -1,21 +1,30 @@
 # -*- coding: utf-8 -*-
-import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from ProductCrawler.spiders.universal_methods.parse_item import parse_item
 
 
 class UastoreSpider(CrawlSpider):
     name = 'uastore'
     allowed_domains = ['store.united-arrows.co.jp']
-    start_urls = ['http://store.united-arrows.co.jp/']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        # matching each item in item list
+        Rule(LinkExtractor(restrict_xpaths='//div[@id="itemList"]'), callback='scrap', follow=False),
+        # matching other pages
+        Rule(LinkExtractor(restrict_xpaths='//div[contains(@id, "pager")]'))
     )
 
-    def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+    def __init__(self, start_urls=None):
+        super(UastoreSpider, self).__init__()
+        if start_urls:
+            self.start_urls = start_urls
+
+    def parse_start_url(self, response):
+        # if response is a item page, parse it use function parse_item.
+        if "goods" in response.url:
+            return parse_item(response, self)
+
+    def call_parse_item(self, response):
+        # parse response use function parse_item
+        return parse_item(response, self)
