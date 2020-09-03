a1d9b6f7588bd1857cd66f350c9904d7e3e56912
Wen <l1048445160@live.com>
2020-09-03 14:13:17 +0800

    fix(ProductCrawler/spiders/kapital.py): Create config file, fix BREAKING CHANGE.

--- ProductCrawler/spiders/kapital.py
+++ ProductCrawler/spiders/kapital.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
-from urllib.parse import urlencode
-
-from scrapy import Request
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
-
-from ProductCrawler.items import ProductItem
-from ProductCrawler.itemloaders import KapitalLoader
+from .generic import GenericSpider
 
 
-class KapitalSpider(CrawlSpider):
+class KapitalSpider(GenericSpider):
     name = 'kapital'
     allowed_domains = ['kapital-webshop.jp']
     rules = [
@@ -28,18 +23,3 @@ class KapitalSpider(CrawlSpider):
             follow=True
         )
     ]
-
-    def __init__(self, start_urls, **kwargs):
-        super().__init__()
-        self.start_urls = start_urls
-
-    def parse_item(self, response):
-        loader = KapitalLoader(item=ProductItem(), response=response)
-        loader.add_value('brand', 'kapital')
-        loader.add_xpath('title', '//h2[@id="itemName"]/text()')
-        loader.add_xpath('art_no', '//p[@class="appeal"]/text()')
-        loader.add_value('item_url', response.url)
-        loader.add_xpath('images', '//div[@class="thumb_list"]//img/@src')
-        loader.add_value('image_base_url', 'https://www.kapital-webshop.jp/')
-        loader.add_value('category', response.url)
-        yield loader.load_item()
