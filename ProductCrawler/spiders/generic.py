5e5a8bdd44013f5021871c007786f8197d9da149
Wen <l1048445160@live.com>
2020-09-03 14:10:50 +0800

    feat: Create new class GenericSpider in model ProductCrawler/spiders/generic.py. BREAKING CHANGE: Delete package ProductCrawler/spiders/universal_methods.

---
+++ ProductCrawler/spiders/generic.py
@@ -0,0 +1,43 @@
+import json
+from scrapy.spiders import CrawlSpider
+from ProductCrawler import items, itemloaders
+
+
+class GenericSpider(CrawlSpider):
+    def __init__(self, **kwargs):
+        super(GenericSpider, self).__init__()
+        self.start_urls = kwargs.pop('start_urls', None)
+        # use spider name get config from package: parse_item_cfg.
+        self.cfg = self._get_spider_cfg()['items']
+        # get Item and ItemLoader class.
+        self.item_class = eval('items.' + self.cfg['class'])
+        self.loader_class = eval('itemloaders.' + self.cfg['loader'])
+
+    def _get_spider_cfg(self):
+        """
+        get configuration.
+        :return: dictionary of configuration
+        """
+        with open('ProductCrawler/spider_configs/{0}.json'.format(self.name), encoding='utf-8') as f:
+            return json.load(f)
+
+    def parse_item(self, response):
+        item = self.item_class()
+        loader = self.loader_class(item=item, response=response)
+
+        # parse response
+        for key, value in self.cfg['attrs'].items():
+            if value["method"] == "":
+                continue
+            if value["method"] == "value":
+                loader.add_value(key, *value["args"])
+            elif value["method"] == "xpath":
+                loader.add_xpath(key, *value["args"])
+            elif value["method"] == "css":
+                loader.add_css(key, *value["args"])
+            elif value["method"] == "eval":
+                loader.add_value(key, *map(eval, value["args"]))
+            else:
+                raise ValueError("No such method: {0}".format(value["method"]))
+
+        return loader.load_item()
