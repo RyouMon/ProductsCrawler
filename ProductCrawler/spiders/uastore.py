d847be4e34caca196af77f3d5d75b9cc29ec699e
Wen <l1048445160@live.com>
2020-09-05 14:31:05 +0800

    fix(ProductCrawler/spiders/kapital.py): Fix BREAKING CHANGE.

--- ProductCrawler/spiders/uastore.py
+++ ProductCrawler/spiders/uastore.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
-from ProductCrawler.spiders.universal_methods.parse_item import parse_item
+from scrapy.spiders import Rule
+from .generic import GenericSpider
 
 
-class UastoreSpider(CrawlSpider):
+class UastoreSpider(GenericSpider):
     name = 'uastore'
     allowed_domains = ['store.united-arrows.co.jp']
 
@@ -15,16 +15,7 @@ class UastoreSpider(CrawlSpider):
         Rule(LinkExtractor(restrict_xpaths='//div[contains(@id, "pager")]'))
     )
 
-    def __init__(self, start_urls=None):
-        super(UastoreSpider, self).__init__()
-        if start_urls:
-            self.start_urls = start_urls
-
     def parse_start_url(self, response):
         # if response is a item page, parse it use function parse_item.
         if "goods" in response.url:
-            return parse_item(response, self)
-
-    def call_parse_item(self, response):
-        # parse response use function parse_item
-        return parse_item(response, self)
+            return self.parse_item(response)
