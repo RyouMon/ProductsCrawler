9285ea10c655502307940a9bb84331a2fe7dd646
Wen <l1048445160@live.com>
2020-09-27 18:06:55 +0800

    improvement(ProductCrawler/spiders/nike.py): Replace CrawlSpider with GenericSpider and spider config.

--- ProductCrawler/spiders/nike.py
+++ ProductCrawler/spiders/nike.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-import scrapy
 from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
-from ProductCrawler.items import ProductItem
-from ProductCrawler.itemloaders import NikeLoader
+from scrapy.spiders import Rule
+from .generic import GenericSpider
 
 
-class NikeSpider(CrawlSpider):
+class NikeSpider(GenericSpider):
     name = 'nike'
     allowed_domains = ['nike.com']
     custom_settings = dict(
@@ -16,37 +14,16 @@ class NikeSpider(CrawlSpider):
     )
 
     rules = (
+        # Match each product in search list
         Rule(
             LinkExtractor(restrict_xpaths=r'//div[contains(@class, "product-grid__items")]', deny=r'.+jpg'),
             callback='parse_item',
             follow=True
         ),
+        # Matching products of other color in product detail
         Rule(
             LinkExtractor(restrict_xpaths=r'//div[@class="colorway-images-wrapper"]', deny=r'.+jpg'),
             callback='parse_item',
             follow=False
         )
-
     )
-
-    def __init__(self, start_urls):
-        super(NikeSpider, self).__init__()
-        self.start_urls = start_urls
-
-    def parse_item(self, response):
-        loader = NikeLoader(item=ProductItem(), response=response)
-        loader.add_value('brand', 'Nike')
-        loader.add_value('title', self.parse_title(response))
-        loader.add_xpath('art_no', '//li[@class="description-preview__style-color ncss-li"]/text()')
-        loader.add_value('item_url', response.url)
-        loader.add_xpath('price', '//div[@class="css-b9fpep"]/text()')
-        loader.add_xpath('images', '//div[@id="pdp-6-up"]//img/@src')
-        loader.add_value('season', None)
-        loader.add_xpath('week', None)
-        yield loader.load_item()
-
-    @staticmethod
-    def parse_title(response):
-        h1 = response.xpath(r'//h1[@id="pdp_product_title"]/text()').get()
-        h2 = response.xpath(r'//h2[@data-test="product-sub-title"]/text()').get()
-        return h1 + ' ' + h2
