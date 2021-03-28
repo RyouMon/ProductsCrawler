7e6e3bf8eedd04e4c386a649417aa8547ea55ae7
Wen <wenslife@outlook.com>
2021-03-28 15:03:52 +0800

    test: remove test to tests

--- tests/test_itemloaders.py
+++ tests/test_itemloaders.py
@@ -15,15 +15,3 @@ class ProductLoaderTest(unittest.TestCase):
             self.loader.delete_url_query_arguments(self.test_url),
             'https://products.com/5S9A1236_1024x1024.jpg',
         )
-
-
-def suite():
-    s = unittest.TestSuite()
-    load_from = unittest.defaultTestLoader.loadTestsFromTestCase
-    s.addTests(load_from(ProductLoaderTest))
-    return s
-
-
-if __name__ == '__main__':
-    t = unittest.TextTestRunner()
-    t.run(suite())
