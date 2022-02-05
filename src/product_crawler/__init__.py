a153fa36b7cf9ea4e5030280278ad242f0ac95a9
Wen <wenslife@outlook.com>
2022-02-05 21:19:59 +0800

    refactor: move entry point to product_crawler package

---
+++ src/product_crawler/__init__.py
@@ -0,0 +1,23 @@
+from argparse import ArgumentParser
+
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from scrapy.spiderloader import SpiderLoader
+
+
+def main():
+    scrapy_settings = get_project_settings()
+    spider_loader = SpiderLoader(scrapy_settings)
+
+    parse = ArgumentParser()
+    parse.add_argument("brand", choices=spider_loader.list())
+    parse.add_argument("start_urls", nargs="+")
+    args = parse.parse_args()
+
+    process = CrawlerProcess(scrapy_settings)
+    process.crawl(args.brand, **{'start_urls': args.start_urls})
+    process.start()
+
+
+if __name__ == '__main__':
+    main()
