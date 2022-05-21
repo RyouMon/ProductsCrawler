6a57c22781a8725d6c0e084b24ae7f8e4a3a6ed4
Wen <wenslife@outlook.com>
2022-05-21 09:54:29 +0800

    style: format code

--- src/products_crawler/spiders/supreme.py
+++ src/products_crawler/spiders/supreme.py
@@ -23,11 +23,7 @@ class SupremeSpider(GenericSpider):
                 self.start_urls.append(url)
 
     def parse_start_url(self, response, **kwargs):
-        """
-        分析当季的页面，生成每一周的请求对象。
-        :param response: 由属性start_urls中url生成的响应对象。
-        :return: 每一周的请求对象，回调方法为parse_week()。
-        """
+        """分析当季的页面，生成每一周的请求对象。"""
         # 得到每一周的相对地址
         weeks = response.xpath(
             r'//div[@class="catalog-inner"]//a/@href'
@@ -35,22 +31,12 @@ class SupremeSpider(GenericSpider):
         # 判断该页面是否应该直接交给parse_week()方法进行解析
         if weeks:
             for week in weeks:
-                yield Request(
-                    url=self.image_base_url+week,
-                    callback=self.parse_week
-                )
+                yield Request(self.image_base_url+week, callback=self.parse_week)
         else:
-            yield Request(
-                url=response.url,
-                callback=self.parse_week
-            )
+            yield self.parse_week(response)
 
     def parse_week(self, response):
-        """
-        分析每一周的页面，返回对每一个商品的请求。
-        :param response: parse_start_url()方法中返回的请求对象对应的响应对象。
-        :return: 对商品的请求对象，回调方法为parse_item()。
-        """
+        """分析每一周的页面，返回对每一个商品的请求。"""
         details = response.xpath('//div[@class="catalog-item-top"]/@data-itemid')
         for detail_no in details:
             yield Request(
