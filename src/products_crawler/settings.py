e2b329d48abe7d5f888b6fc51c51f6696614d19b
Wen <wenslife@outlook.com>
2022-05-22 14:28:10 +0800

    fix: Fix settings for pipelines (rename IMAGES_STORE to FILES_STORE). Add USER_AGENT. Remove comments.

--- src/products_crawler/settings.py
+++ src/products_crawler/settings.py
@@ -1,116 +1,29 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for products_crawler project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-
 BOT_NAME = 'products_crawler'
 
 SPIDER_MODULES = ['products_crawler.spiders']
 NEWSPIDER_MODULE = 'products_crawler.spiders'
 
-IMAGES_STORE = r'products'
+FILES_STORE = r'products'
 
 SELENIUM_TIMEOUT = 20
 CHROME_OPTIONS = ['--headless']
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = 'products_crawler (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36'
 
-# Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
-
-# Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
 DOWNLOAD_DELAY = 0.1
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
 
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+TELNETCONSOLE_ENABLED = False
 
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
-
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
-
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'products_crawler.middlewares.ProductCrawlerSpiderMiddleware': 543,
-#}
-
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'products_crawler.middlewares.ProductCrawlerDownloaderMiddleware': 543,
-#}
-
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
-
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'products_crawler.pipelines.ProductInfoPipeline': 300,
     'products_crawler.pipelines.ProductImagesPipeline': 301,
 }
 
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
-# The initial download delay
 AUTOTHROTTLE_START_DELAY = 0.2
-# The maximum download delay to be set in case of high latencies
 AUTOTHROTTLE_MAX_DELAY = 0.5
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
 AUTOTHROTTLE_TARGET_CONCURRENCY = 3.0
-# Enable showing throttling stats for every response received:
-# AUTOTHROTTLE_DEBUG = True
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
-
-# MAIL_FROM = 'example@host'
-
-# MAIL_HOST = 'smtp.host'
-
-MAIL_PORT = 465
-
-# MAIL_USER = 'example@host
-
-# MAIL_PASS = ''
-
-MAIL_TLS = True
-
-MAIL_SSL = True
-
-# STATSMAILER_RCPTS = []
-
-
-EXTENSIONS = {
-    'scrapy.extensions.statsmailer.StatsMailer': 500,
-}
